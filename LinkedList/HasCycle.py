5efc9de053330865ffc0154a79b9b2936cd80ffe
neerajmenon <neeraj.menon.m@gmail.com>
2023-03-24 20:08:27 -0400

    Adding leetcode solutions

---
+++ LinkedList/HasCycle.py
@@ -0,0 +1,18 @@
+#LC-141
+#Check if linked list has a cycle
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        slow,fast = head,head
+        while fast!=None and fast.next!=None:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
