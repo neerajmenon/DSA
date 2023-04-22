4f6c9721503a273fd19b90b6422ea1f94c380d1d
neerajmenon <neeraj.menon.m@gmail.com>
2023-04-22 07:43:15 -0400

    pushing latest

--- Trees/BT_inorder.py
+++ Trees/BT_inorder.py
@@ -16,35 +16,17 @@ class Solution(object):
         dfs(root)
         return arr
     
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution(object):
     def inorderTraversal(self, root):
-        arr=[]
-        s=[] #Stack
-
-        while root:
-            
-            if len(s)>0 and s[-1]==root:
-                s.pop()
-
-            elif root.left:
-                s.append(root)
+        res = []
+        stack = []
+        
+        while root or stack:
+            while root:
+                stack.append(root)
                 root = root.left
-                continue
-            arr.append(root.val)
-            if root.right:
-                root = root.right
-                continue
-
-            if len(s)>0:
-                root = s[-1]
-            else:
-                break
-
-
-        return arr
+            root = stack.pop()
+            res.append(root.val)
+            root = root.right
+        
+        return res
