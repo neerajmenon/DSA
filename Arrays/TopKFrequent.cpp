e5f54c922715af32a62817bc9239e6c7936ca192
neerajmenon <neeraj.menon.m@gmail.com>
2023-05-07 08:53:51 -0400

    Updating efficient solution for TopKFrequent

--- Arrays/TopKFrequent.cpp
+++ Arrays/TopKFrequent.cpp
@@ -3,6 +3,37 @@
 
 using namespace std;
 
+//Solution with map and bucket sort
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> count;
+        vector<vector<int>> freq(nums.size() + 1);
+
+        for (int n : nums) {
+            count[n] = 1 + count[n];
+        }
+
+        for (auto& p : count) {
+            freq[p.second].push_back(p.first);
+        }
+
+        vector<int> res;
+        for (int i = freq.size() - 1; i > 0 && res.size() < k; --i) {
+            for (int n : freq[i]) {
+                res.push_back(n);
+                if (res.size() == k) {
+                    return res;
+                }
+            }
+        }
+
+        return res;
+    }
+};
+
+
+//Solution with map only
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
