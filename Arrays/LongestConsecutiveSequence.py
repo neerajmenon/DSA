c95746e6144df47d4f6f7061cf7dde11fa3d15a8
neerajmenon <neeraj.menon.m@gmail.com>
2023-05-14 16:08:43 -0400

    Optimizing LCS solution

--- Arrays/LongestConsecutiveSequence.py
+++ Arrays/LongestConsecutiveSequence.py
@@ -1,3 +1,4 @@
+# O(n) time and space
 class Solution(object):
     def longestConsecutive(self, nums):
         longest = 0
@@ -11,3 +12,20 @@ class Solution(object):
                 longest = max(length,longest)
         
         return longest
+    
+# O(nlog(n)) time O(1) space    
+class Solution:
+    def longestConsecutive(self, nums):
+        if len(nums) == 0:
+            return 0
+        nums.sort()
+        max_length = 1
+        current_length = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                if nums[i] - nums[i-1] == 1:
+                    current_length += 1
+                else:
+                    max_length = max(max_length, current_length)
+                    current_length = 1
+        return max(max_length, current_length)
